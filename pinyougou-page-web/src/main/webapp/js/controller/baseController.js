1e6023eddd3964d2aba74709ca754c1b14cd6583
qunan <1023423884@qq.com>
2018-07-08 13:18:35 +0800

    page error

--- pinyougou-page-web/src/main/webapp/js/controller/baseController.js
+++ pinyougou-page-web/src/main/webapp/js/controller/baseController.js
@@ -13,7 +13,7 @@ app.controller('baseController' ,function($scope){
          currentPage: 1,
          totalItems: 10,
          itemsPerPage: 10,
-         perPageOptions: [10, 20, 30, 40, 50],loadSku
+         perPageOptions: [10, 20, 30, 40, 50],
          onChange: function(){
         	 $scope.reloadList();//重新加载
      	 }
